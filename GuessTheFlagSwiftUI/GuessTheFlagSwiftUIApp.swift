3c79d2f32628b778c2d1f92e236d866c558e5e08
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2023-12-26 05:59:33 -0600

    Initial Commit

---
+++ GuessTheFlagSwiftUI/GuessTheFlagSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagSwiftUIApp.swift
+//  GuessTheFlagSwiftUI
+//
+//  Created by Luis Enrique Rosas Espinoza on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
